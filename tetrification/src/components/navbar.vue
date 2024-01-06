374d774cd1ce825fbcc2e73ca92ecb6da45627ab
sofiapcabral <s.sofia.c.cabral@gmail.com>
2024-01-06 11:05:34 +0000

    dashboard & navbar

---
+++ tetrification/src/components/navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app width="125" height="100" style="background-color: var(--vt-c-dark-purple-2);">
+      <v-container class="text-center">
+        <img src="@/assets/images/navbar/logo.svg" class="my-12">
+        <v-list>
+          <v-list-item class="mb-4">
+            <v-list-item-icon>
+              <v-btn :to="{ name: 'dashboard' }" :rounded="1" class="my-2 btn" style="height: 60px; background-color: var(--vt-c-dark-purple-2);" :elevation="0"><img src="@/assets/images/navbar/dashboard.svg"></v-btn>
+            </v-list-item-icon>
+          </v-list-item>
+          <v-list-item class="mb-4">
+            <v-list-item-icon>
+              <v-btn :to="{ name: 'leaderboard' }" :rounded="1" class="my-2 btn" style="height: 60px; background-color: var(--vt-c-dark-purple-2);" :elevation="0"><img src="@/assets/images/navbar/leaderboard.svg"></v-btn>
+            </v-list-item-icon>
+          </v-list-item>
+          <v-list-item class="mb-4">
+            <v-list-item-icon>
+              <v-btn :to="{ name: 'profile' }" :rounded="1" class="my-2 btn" style="height: 60px; background-color: var(--vt-c-dark-purple-2);" :elevation="0"><img src="@/assets/images/navbar/profile.svg"></v-btn>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list>
+      </v-container>
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+import { useUserStore } from '@/stores/users';
+export default {
+  data() {
+    return {
+      userStore: useUserStore(),
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
--- tetrification/src/views/DashboardView.vue
+++ tetrification/src/views/DashboardView.vue
@@ -1,13 +1,267 @@
 <template>
-  
+  <v-container class="dashboard">
+    <v-row>
+      
+      <!-- Navbar -->
+      <v-col cols="2">
+        <NavBar></NavBar>
+      </v-col>
+
+      <!-- middle side content -->
+      <v-col cols="7" md="6">
+        <div>
+          <h1>Dashboard</h1>
+          <h3>Welcome back, <span>{{userStore.getUser.username}}</span>.</h3>
+        </div>
+        <div class="streak">
+          <img :src="userStore.getUser.streakImageSrc" alt="Streak Image">
+          <h4>{{ `x${userStore.getUser.streak}` }}</h4>
+        </div>
+
+        <!-- Live card -->
+        <v-card class="pa-4 pt-4 mb-8 mt-4" elevation="4" style="z-index: 1; background-color: var(--vt-c-light-purple-1)">
+          <v-card-title class="text-center card-title mb-4" style="background-color: var(--vt-c-medium-purple-2); max-width: 150px; margin: 0 auto;">
+            Live
+          </v-card-title>
+          <v-card v-for="(game, index) in liveGames" :key="index" class="pa-2 pt-8 mb-4 game" style="z-index: 1;" @click="redirectToGameDetails(game.gameId)">
+            <!-- button simulating image; elements positioning purposes -->
+            <v-btn></v-btn>
+            <span>{{ game.player1 }}</span>
+            <h3>{{ game.pointsPlayer1 }}</h3>
+            <span>vs</span>
+            <h3>{{ game.pointsPlayer2 }}</h3>
+            <span>{{ game.player2 }}</span>
+            <v-btn></v-btn>
+          </v-card>
+        </v-card>
+
+        <!-- Top Seeds card -->
+        <v-card class="pa-4 pt-4" elevation="4" style="z-index: 1; background-color: var(--vt-c-white-soft)" :to="{name: 'leaderboard'}">
+          <v-card-title class="text-center card-title mb-4">
+            Top Seeds
+          </v-card-title>
+          <v-row>
+            <v-col cols="4">
+              <v-card class="mb-4 pb-4" style="z-index: 1;">
+                <v-card-title class="text-center card-title">#1</v-card-title>
+                <v-card-subtitle class="text-center">SIDNEV</v-card-subtitle>
+              </v-card>
+            </v-col>
+            <v-col cols="4">
+              <v-card class="mb-4 pb-4" style="z-index: 1;">
+                <v-card-title class="text-center card-title">#2</v-card-title>
+                <v-card-subtitle class="text-center">DENGLER</v-card-subtitle>
+              </v-card>
+            </v-col>
+            <v-col cols="4">
+              <v-card class="mb-4 pb-4" style="z-index: 1;">
+                <v-card-title class="text-center card-title">#3</v-card-title>
+                <v-card-subtitle class="text-center">FRACTAL</v-card-subtitle>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+
+      <!-- right side -->
+      <v-col class="game-list ml-auto text-center" cols="3">
+        <v-text-field v-model="searchQuery" label="Search" outlined dense class="mt-4 pa-4" @click="handleBtnClick" @input="searchGames"></v-text-field>
+        <h4 class="pa-4 font-weight-bold">Games</h4>
+        <v-btn class="ma-4 btn-game-list btn-active" @click="handleBtnClick('past')" id="btnPast">Past</v-btn>
+        <v-btn class="ma-4 btn-game-list" @click="handleBtnClick('future')" id="btnFuture">Future</v-btn>
+        <div>
+          <v-card class="mb-4 py-4 ma-4 game-card" v-if="visibleGames.length > 0" v-for="(game, index) in visibleGames" :key="index">
+            <div style="display: flex; justify-content: space-evenly;" class="ma-2">
+              <v-btn></v-btn>
+              <span style="width: 100px;">{{ game.player1 }}</span>
+              <div style="display: flex; justify-content: space-around;">
+                <img src="@/assets/images/life1.svg"  style="width: 30%;">
+                <img src="@/assets/images/life1.svg" style="width: 30%;">
+                <img src="@/assets/images/life1.svg" style="width: 30%;">
+              </div>
+            </div>
+            <div style="display: flex; justify-content: space-evenly;" class="mt-6">
+              <v-btn></v-btn>
+              <span style="width: 100px;">{{ game.player2 }}</span>
+              <div style="display: flex; justify-content: space-around;">
+                <img src="@/assets/images/life1.svg" style="width: 30%;">
+                <img src="@/assets/images/life1.svg" style="width: 30%;">
+                <img src="@/assets/images/life1.svg" style="width: 30%;">
+              </div>
+            </div>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
+import { useUserStore } from '@/stores/users';
+import { useGameStore } from '@/stores/games'
+import NavBar from '@/components/navbar.vue'
+import games from '@/api/mocks/games';
 
+export default {
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      userStore: useUserStore(),
+      gameStore: useGameStore(),
+      searchQuery: '',
+      liveGames: [],
+      visibleGames: [],
+    };
+  },
+  methods: {
+    games() {
+      return this.gameStore.getGames;
+    },
+    redirectToGameDetails(gameId) {
+      this.$router.push(`/dashboard/game/${gameId}`);
+    },
+    handleBtnClick(type) {
+      const btnPast = document.getElementById('btnPast');
+      const btnFuture = document.getElementById('btnFuture');
+
+      if (type === 'past') {
+        btnPast.classList.add('btn-active');
+        btnFuture.classList.remove('btn-active');
+        this.searchQuery = '';
+        this.visibleGames = this.getPastGames();
+      } else if (type === 'future') {
+        btnFuture.classList.add('btn-active');
+        btnPast.classList.remove('btn-active');
+        this.searchQuery = '';
+        this.visibleGames = this.getFutureGames();
+      } else {
+        btnPast.classList.remove('btn-active');
+        btnFuture.classList.remove('btn-active');
+        this.visibleGames = [];
       }
+    },
+    getLiveGames() {
+      const liveGameIds = [5, 6];
+      console.log('All games:', this.gameStore.getGames);
+      
+      const liveGames = liveGameIds.map(gameId => {
+        const gamesId = this.gameStore.getGames.filter(game => game.gameId === gameId);
+        return gamesId.length > 0 ? gamesId[0] : null;
+      }).filter(game => game !== null);
+
+      console.log('Live games:', liveGames);
+      return liveGames;
+    },
+    getPastGames() {
+      const pastGameIds = [1, 2, 3, 4];
+      console.log('All games:', this.gameStore.getGames);
+      
+      const pastGames = pastGameIds.map(gameId => {
+        const gamesId = this.gameStore.getGames.filter(game => game.gameId === gameId);
+        return gamesId.length > 0 ? gamesId[gamesId.length - 1] : null;
+      }).filter(game => game !== null);
+
+      console.log('Past games:', pastGames);
+      return pastGames;
+    },
+    getFutureGames() {
+      const futureGameIds = [7];
+      console.log('All games:', this.gameStore.getGames);
+      
+      const futureGames = futureGameIds.map(gameId => {
+        const gamesId = this.gameStore.getGames.filter(game => game.gameId === gameId);
+        return gamesId.length > 0 ? gamesId[0] : null;
+      }).filter(game => game !== null);
+
+      console.log('Future games:', futureGames);
+      return futureGames;
+    },
+    searchGames() {
+      const searchPlayer = this.searchQuery.toLowerCase().trim();
+
+      if (searchPlayer.length === 0) {
+        this.visibleGames = [];
+        return;
+      }
+
+      this.visibleGames = this.games().filter(game => {
+        const player1 = game.player1.toLowerCase().includes(searchPlayer);
+        const player2 = game.player2.toLowerCase().includes(searchPlayer);
+
+        if ([1, 2, 3, 4].includes(game.gameId)) {
+          // Retrieve the last object of the array for gameId 1-4
+          const filteredGames = this.gameStore.getGames.filter(g => g.gameId === game.gameId);
+          return player1 || player2 && filteredGames.length > 0 ? game === filteredGames[filteredGames.length - 1] : false;
+        } else if ([5, 6, 7].includes(game.gameId)) {
+          // Retrieve the first object of the array for gameId 5-7
+          const filteredGames = this.gameStore.getGames.filter(g => g.gameId === game.gameId);
+          return player1 || player2 && filteredGames.length > 0 ? game === filteredGames[0] : false;
+        }
+
+        return false; 
+      });
+    },
+  },
+  created() {
+    if (this.games().length === 0) {
+      this.gameStore.fetchGames().then(() => {
+        this.liveGames = this.getLiveGames();
+        this.visibleGames = this.getPastGames();
+      });
+    } else {
+      this.liveGames = this.getLiveGames();
     }
-  }
+  },
+};
 </script>
+
+<style scoped>
+  .dashboard {
+    width: 100%;
+    max-width: 1550px;
+  }
+
+  h3 span {
+    color: var(--vt-c-medium-purple-2);
+  }
+
+  .streak {
+    display: flex;
+  }
+
+  .streak h4 {
+    font-size: 4vh;
+    color: #FBD73A;
+  }
+
+  .game-list {
+    background-color: var(--vt-c-white-mute); 
+    padding: 0; 
+  }
+
+  .btn-game-list {
+    width: 40%;
+    background-color: var(--vt-c-white-mute);
+    color: var(--vt-c-dark-purple-2);
+    box-shadow: none;
+  }
+
+  .btn-game-list:hover, .btn-active  {
+    background-color: var(--vt-c-dark-purple-2);
+    color: var(--vt-c-white-mute);
+  }
+
+  .game {
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  .game-card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+</style>
